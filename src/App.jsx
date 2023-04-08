9f672fa84ef109c11051d4c5a0bdf87a95418810
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-08 15:24:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Humaion Kobir</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, unde.</p>
+    </div>
+  )
+}
+
+export default App
